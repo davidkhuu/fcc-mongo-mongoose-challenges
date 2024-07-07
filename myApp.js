1a11d3f23e5711364ec213370f336f2bf5752cd3
David Khuu <davidkhuu.dk@gmail.com>
2024-07-06 22:19:04 -0600

    Create a model (person schema)

--- myApp.js
+++ myApp.js
@@ -2,7 +2,20 @@ require('dotenv').config();
 let mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+  },
+  favoriteFoods: {
+    type: [String],
+  },
+});
+
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
